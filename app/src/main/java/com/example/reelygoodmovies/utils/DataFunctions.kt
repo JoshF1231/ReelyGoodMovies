5aa348d14720e6c5a361f08941aebe5ea60df46b
Josh <Joshfiquette@gmail.com>
2025-02-15 20:56:46 +0200

    Adding Movie Repository Util to check local and remote DB

---
+++ app/src/main/java/com/example/reelygoodmovies/utils/DataFunctions.kt
@@ -0,0 +1,25 @@
+package com.example.reelygoodmovies.utils
+
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.Dispatchers
+import androidx.lifecycle.liveData
+import androidx.lifecycle.map
+
+fun <T,A> performFetchingAndSaving(localDbFetch : () -> LiveData<T>,
+                                   remoteDbFetch: suspend () -> Resource <A>,
+                                   localDbSave: suspend (A) -> Unit) : LiveData<Resource<T>> =
+    liveData (Dispatchers.IO){
+        emit(Resource.loading())
+        val source = localDbFetch().map { Resource.success(it) }
+        emitSource(source)
+
+        val fetchResource = remoteDbFetch()
+
+        if (fetchResource.status is Success)
+            localDbSave(fetchResource.status.data!!)
+
+        else if (fetchResource.status is Error){
+            emit(Resource.error(fetchResource.status.message))
+            emitSource(source)
+        }
+    }
